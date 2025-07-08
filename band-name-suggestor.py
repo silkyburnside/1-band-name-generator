91c6860ddfa8ea53702fd385579e85d413545387
Silky <silkyburnside@gmail.com>
2025-07-08 19:08:36 +1000

    added line breaks

--- band-name-suggestor.py
+++ band-name-suggestor.py
@@ -1,4 +1,4 @@
 print("Band name suggestor...")
-city = input("What city did you grow up in?")
-pet = input("What is the name of your pet?")
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
 print("Suggested band name: " + city + "-" + pet + "s")
