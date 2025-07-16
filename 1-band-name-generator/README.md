1c42a62dbf872bc295425f86798528547b65d0cd
Silky <silkyburnside@gmail.com>
2025-07-16 20:13:07 +1000

    Delete 1-band-name-generator directory

--- 1-band-name-generator/README.md
+++
@@ -1,2 +0,0 @@
-## Day 1
-Band name generataor using print and input functions in python. 
--- 1-band-name-generator/band-name-generator.py
+++
@@ -1,4 +0,0 @@
-print("Band name suggestor...")
-city = input("What city did you grow up in?\n")
-pet = input("What is the name of your pet?\n")
-print("Suggested band name: " + city + "-" + pet + "s")
